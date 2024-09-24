414c3a5365c671abe02bb035c340938af1f8f916
Arnold10-web <arnoldakandinda@gmail.com>
2024-09-24 10:50:43 +0300

    Initial commit

--- js/main.js
+++ js/main.js
@@ -7,15 +7,17 @@ closeBtn.addEventListener('click', closeModal)
 window.addEventListener('click', outsideClick)
 
 function openModal() {
-  modal.style.display = 'block'
+
+    modal.style.display ='block'
 }
 
 function closeModal() {
-  modal.style.display = 'none'
+    modal.style.display ='none'
 }
 
 function outsideClick(e) {
-  if (e.target == modal) {
-    closeModal()
-  }
+    if (e.target == modal) {
+
+        closeModal()
+    }
 }
